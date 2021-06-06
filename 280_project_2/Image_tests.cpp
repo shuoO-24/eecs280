e2ed4eeb5e29f354b17721c604da6e835d33eb41
Aravind Mantravadi <aravind1338@gmail.com>
2021-06-06 15:59:14 -0400

    Unit tests for image module

--- 280_project_2/Image_tests.cpp
+++ 280_project_2/Image_tests.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <sstream>
 #include <cassert>
+#include <fstream>
 
 using namespace std;
 
@@ -47,6 +48,141 @@ TEST(test_print_basic) {
 
 // IMPLEMENT YOUR TEST FUNCTIONS HERE
 // You are encouraged to use any functions from Image_test_helpers.h as needed.
+
+
+
+// Creates a 4x5 (row x columns) Image and checks
+// if image height and width are correct
+
+TEST(test_height_width) {
+
+  Image* img = new Image;
+
+  Image_init(img, 5, 4);
+
+  ASSERT_EQUAL(Image_height(img), 4);
+  ASSERT_EQUAL(Image_width(img), 5);
+
+  delete img;
+
+}
+
+// Reads image from PPM file and checks
+// if dimensions are correct
+
+TEST(test_height_width_ppm) {
+
+  Image* img = new Image;
+
+  string input_filename = "crabster_50x45.correct.ppm";
+
+  std::ifstream fin(input_filename);
+
+  Image_init(img, fin);
+
+  ASSERT_EQUAL(Image_height(img), 45);
+  ASSERT_EQUAL(Image_width(img), 50);
+
+  delete img;
+
+}
+
+// Reads image from PPM file
+// Checks if get_pixel works as intended
+
+TEST(test_get_pixel) {
+
+  Image* img = new Image;
+
+  string input_filename = "crabster_50x45.correct.ppm";
+
+  std::ifstream fin(input_filename);
+
+  Image_init(img, fin);
+
+  Pixel p1 = Image_get_pixel(img, 0, 0);
+  Pixel p2 = Image_get_pixel(img, 15, 2);
+  Pixel p3 = Image_get_pixel(img, 44, 49);
+
+  ASSERT_TRUE(Pixel_equal(p1, {255, 255, 255}));
+  ASSERT_TRUE(Pixel_equal(p2, {149, 45, 57}));
+  ASSERT_TRUE(Pixel_equal(p3, {255, 255, 255}));
+  
+  delete img;
+
+}
+
+// Create a 4x3 image
+// Check if Image_fill works correctly
+
+TEST(test_fill) {
+
+  Image* img = new Image;
+
+  Image_init(img, 3, 4);
+  Image_fill(img, {3, 4, 5});
+
+  string output_correct = "P3\n3 4\n255\n3 4 5 3 4 5 3 4 5 \n3 4 5 3 4 5 3 4 5 \n3 4 5 3 4 5 3 4 5 \n3 4 5 3 4 5 3 4 5 \n";
+  std::ostringstream ss_output;
+  Image_print(img, ss_output);
+  string actual = ss_output.str();
+
+  ASSERT_EQUAL(actual, output_correct);
+
+  delete img;
+}
+
+// Create two 4x3 images, initialize with ppm
+// check if set_pixel works
+
+TEST(test_set_pixel) {
+
+  Image* img = new Image;
+  Image* img2 = new Image;
+
+  string input_filename = "crabster_50x45.correct.ppm";
+  std::ifstream fin(input_filename);
+  Image_init(img, fin);
+
+  Image_init(img2, Image_width(img), Image_height(img));
+
+  Image_fill(img2, {10, 2, 6});
+
+  for (size_t r = 0; r < Image_height(img); ++r) {
+    for(size_t c = 0; c < Image_width(img); ++c) {
+      Image_set_pixel(img, r, c, {10, 2, 6});
+    }
+  }
+  
+  ASSERT_TRUE(Pixel_equal(Image_get_pixel(img, 2, 1), {10, 2, 6}));
+  ASSERT_TRUE(Image_equal(img, img2));
+
+  delete img;
+  delete img2;
+
+}
+
+// Create a 1x1 image and check
+// if Image_print works
+
+TEST(test_print) {
+
+  Image* img = new Image;
+
+  Image_init(img, 1, 1);
+  Image_set_pixel(img, 0, 0, {3, 2, 6});
+
+  string output_correct = "P3\n1 1\n255\n3 2 6 \n";
+  std::ostringstream ss_output;
+  Image_print(img, ss_output);
+  string actual = ss_output.str();
+
+  ASSERT_EQUAL(actual, output_correct);
+
+  delete img;
+
+}
+
 
 // NOTE: The unit test framework tutorial in Lab 2 originally
 // had a semicolon after TEST_MAIN(). Although including and
