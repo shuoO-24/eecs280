7c6fddae3af965296caf2c8948d53720b7a36f41
Aravind Mantravadi <aravind1338@gmail.com>
2021-06-26 17:07:15 -0400

    Starter files with function stubs

---
+++ 280_project_3/Pack.cpp
@@ -0,0 +1,48 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <array>
+#include "Card.h"
+#include "Pack.h"
+
+// EFFECTS: Initializes the Pack to be in the following standard order:
+//          the cards of the lowest suit arranged from lowest rank to
+//          highest rank, followed by the cards of the next lowest suit
+//          in order from lowest to highest rank, and so on.
+// NOTE: The standard order is the same as that in pack.in.
+// NOTE: Do NOT use pack.in in your implementation of this function
+Pack::Pack() {
+    assert(false);
+}
+
+// REQUIRES: pack_input contains a representation of a Pack in the
+//           format required by the project specification
+// MODIFIES: pack_input
+// EFFECTS: Initializes Pack by reading from pack_input.
+Pack::Pack(std::istream& pack_input) {
+    assert(false);
+}
+
+// REQUIRES: cards remain in the Pack
+// EFFECTS: Returns the next card in the pack and increments the next index
+Card Pack::deal_one() {
+    assert(false);
+}
+
+// EFFECTS: Resets next index to first card in the Pack
+void Pack::reset() {
+    assert(false);
+    assert(next);
+}
+
+// EFFECTS: Shuffles the Pack and resets the next index. This
+//          performs an in shuffle seven times. See
+//          https://en.wikipedia.org/wiki/In_shuffle.
+void Pack::shuffle() {
+    assert(false);
+}
+
+// EFFECTS: returns true if there are no more cards left in the pack
+bool Pack::empty() const {
+    assert(false);
+}
---
+++ 280_project_3/Player.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include "Card.h"
+#include "Player.h"
+
+
+Player * Player_factory(const std::string &name, const std::string &strategy) {
+    assert(false);
+}
+
+//EFFECTS: Prints player's name to os
+std::ostream & operator<<(std::ostream &os, const Player &p) {
+    assert(false);
+}
